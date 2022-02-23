818892716a69927cd5b8257ef106287b24441329
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-23 19:09:03 +0300

    Дополнение репозитория методами спецификации

--- Infrastructure/Data/GenericRepository.cs
+++ Infrastructure/Data/GenericRepository.cs
@@ -23,11 +23,15 @@ namespace Infrastructure.Data
         }
         public async Task<T> GetEntityWithSpec(ISpecification<T> spec)
         {
-            throw new NotImplementedException();
+            return await ApplySpecification(spec).FirstOrDefaultAsync();
         }
         public async Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec)
         {
-            throw new NotImplementedException();
+            return await ApplySpecification(spec).ToListAsync();
+        }
+        private IQueryable<T> ApplySpecification(ISpecification<T> spec)
+        {
+            return SpecificationEvaluator<T>.GetQuery(_context.Set<T>().AsQueryable(), spec);
         }
     }
 }
