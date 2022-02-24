0730663d624106e19cbe20b18200896d5cbdd406
ZamanovRenat <zamanov.renat@mail.ru>
2022-02-24 11:26:53 +0300

    Промежуточное ПО обработки ошибок

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -6,6 +6,8 @@ using Core.Specifications;
 using Microsoft.EntityFrameworkCore;
 using API.Dtos;
 using AutoMapper;
+using API.Errors;
+using Microsoft.AspNetCore.Http;
 
 namespace API.Controllers
 {
@@ -37,11 +39,15 @@ namespace API.Controllers
             return Ok(_mapper.Map<IReadOnlyList<ProductToReturnDto>>(products));
         }
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productsRepo.GetEntityWithSpec(spec);
+
+            if (product == null) return NotFound(new ApiResponse(404));
 
             return _mapper.Map<ProductToReturnDto>(product);
         }
